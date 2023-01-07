b49ba53a4f46fdf05ce7e46b59c82311cb8b60d1
AlpatyevNikita <chococacaococonut@gmail.com>
2023-01-07 19:38:54 +0300

    fixed right subview layout ( add subview directly to contetView, but container already deleted)

--- settingsClone/settingsClone/Sources/Views/CustomTableViewCell.swift
+++ settingsClone/settingsClone/Sources/Views/CustomTableViewCell.swift
@@ -28,9 +28,7 @@ final class CustomTableViewCell: UITableViewCell {
     private lazy var leftImage = UIImageView()
     
     private lazy var mainTitle = UILabel()
-    
-    private lazy var rightSubview = UIView()
-    
+        
     private lazy var subtitle = UILabel()
     
     private lazy var badges = UILabel()
@@ -63,7 +61,6 @@ final class CustomTableViewCell: UITableViewCell {
         
         contentView.addSubview(leftImage)
         contentView.addSubview(mainTitle)
-        contentView.addSubview(rightSubview)
     }
     
     private func updateSubviews(_ frame: CGRect) {
@@ -72,16 +69,6 @@ final class CustomTableViewCell: UITableViewCell {
         setupRightSubview()
         
         separatorInset.left = mainTitle.frame.minX
-        
-        if let switchState = internalData.switcher {
-            setupSwitcher(with: switchState)
-        } else if let subtitleText = internalData.subtitle {
-            setupSubtitle(with: subtitleText)
-        } else if let badgeCount = internalData.badges {
-            setupBadges(with: badgeCount)
-        } else {
-            rightSubview.subviews.forEach { $0.removeFromSuperview() }
-        }
     }
     
     private func setupImage(_ frame: CGRect) {
@@ -112,19 +99,19 @@ final class CustomTableViewCell: UITableViewCell {
         mainTitle.snp.removeConstraints()
         mainTitle.snp.makeConstraints { make in
             make.left.equalTo(leftImage.snp.right).offset(divider)
-            make.right.equalTo(rightSubview)
+            make.right.equalTo(contentView)
             make.height.equalTo(contentView).multipliedBy(isLoginCell ? 0.32 : 1)
             make.bottom.equalTo(contentView).multipliedBy(isLoginCell ? 0.5 : 1)
         }
     }
     
     private func setupRightSubview() {
-        rightSubview.snp.removeConstraints()
-        rightSubview.snp.makeConstraints { make in
-            make.height.equalTo(contentView)
-            make.width.equalTo(contentView).multipliedBy(0.25)
-            make.right.equalTo(contentView)
-            make.centerY.equalTo(contentView)
+        if let switchState = internalData.switcher {
+            setupSwitcher(with: switchState)
+        } else if let subtitleText = internalData.subtitle {
+            setupSubtitle(with: subtitleText)
+        } else if let badgeCount = internalData.badges {
+            setupBadges(with: badgeCount)
         }
     }
     
@@ -132,12 +119,12 @@ final class CustomTableViewCell: UITableViewCell {
         switcher.isOn = state
         switcher.addTarget(self, action: #selector(switchToggled), for: .valueChanged)
         
-        rightSubview.addSubview(switcher)
+        contentView.addSubview(switcher)
         switcher.snp.removeConstraints()
         switcher.snp.makeConstraints { make in
-            make.centerY.equalTo(rightSubview)
+            make.centerY.equalTo(contentView)
             make.width.lessThanOrEqualTo(contentView.frame.height)
-            make.right.equalTo(rightSubview).inset(divider * 2)
+            make.right.equalTo(contentView).inset(divider * 2)
         }
     }
     
@@ -151,11 +138,11 @@ final class CustomTableViewCell: UITableViewCell {
         badges.layer.masksToBounds = true
         badges.layer.cornerRadius = contentView.frame.height * 0.28
         
-        rightSubview.addSubview(badges)
+        contentView.addSubview(badges)
         badges.snp.removeConstraints()
         badges.snp.makeConstraints { make in
             make.width.height.equalTo(contentView.snp.height).multipliedBy(0.56)
-            make.right.equalTo(contentView)
+            make.right.equalTo(contentView).inset(divider)
             make.centerY.equalTo(contentView)
         }
     }
@@ -173,7 +160,7 @@ final class CustomTableViewCell: UITableViewCell {
             subtitle.snp.removeConstraints()
             subtitle.snp.makeConstraints { make in
                 make.left.equalTo(leftImage.snp.right).offset(divider)
-                make.right.equalTo(rightSubview)
+                make.right.equalTo(contentView)
                 make.height.equalTo(contentView).multipliedBy(0.32)
                 make.top.equalTo(mainTitle.snp.bottom)
             }
@@ -182,11 +169,11 @@ final class CustomTableViewCell: UITableViewCell {
             subtitle.textAlignment = .right
             subtitle.font = .systemFont(ofSize: 17, weight: .regular)
             
-            rightSubview.addSubview(subtitle)
-            rightSubview.snp.removeConstraints()
+            contentView.addSubview(subtitle)
+            contentView.snp.removeConstraints()
             subtitle.snp.makeConstraints { make in
                 make.left.equalTo(leftImage.snp.right)
-                make.right.equalTo(contentView)
+                make.right.equalTo(contentView).inset(divider / 2)
                 make.height.equalTo(contentView)
                 make.centerY.equalTo(contentView)
             }
